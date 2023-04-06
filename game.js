bbde4a7029937e08b01c939807a7e0a7973a6b76
davis0011 <Davis85754556>
2023-04-06 22:02:36 +0300

    did the register with all of the checkings . needs to finish the saving and the searching proccess in the register/login

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var list = []
+    var users = [];
     $('#logo').animate({opacity: 0.01}, 500, function () {
 
         $(this).hide();
@@ -14,21 +14,101 @@ $(document).ready(function () {
         $('#landing').fadeOut();
         $('#register').delay(500).show(0);
     });
+    var number = /([0-9])/;
+    var alphabets = /([a-zA-Z])/;
+    var pwok = false;
+    var fnok = false;
+    var lnok = false;
+    var emailok = false;
+    $('#pw1Reg').keyup(function(){
+        let pw1 = $('#pw1Reg').val().trim();
+        if(pw1.length < 8){
+            $('#password-strength-status1').html("Weak (should be atleast 8 characters.)");
+        }
+        else{
+            if ((pw1.match(number) && pw1.match(alphabets))) {
+			    $('#password-strength-status1').html("Strong");
+            }
+            else{
+                $('#password-strength-status1').html("please use numbers and characters");
+            }
+        }
+    });
+    $('#pw2Reg').keyup(function(){
+        let pw2 = $('#pw2Reg').val().trim();
+        let pw1 = $('#pw1Reg').val().trim();
+        if(pw1 != pw2){
+            $('#password-strength-status2').html("does not match the password above");
+            pwok = false;
+        }
+        else{
+            $('#password-strength-status2').html("");
+            pwok = true;
+        }
+    });
+    $('#fnameReg').keyup(function(){
+        let fname = $('#fnameReg').val().trim();
+        if(fname.match(number)){
+            $('#fnamealert').html("Please dont put numbers in your first name");
+            fnok = false;
+        }
+        else{
+            $('#fnamealert').html("");
+            fnok = true;
+        }
+    });
+    $('#lnameReg').keyup(function(){
+        let lname = $('#lnameReg').val().trim();
+        if(lname.match(number)){
+            $('#lnamealert').html("Please dont put numbers in your last name");
+            lnok = false;
+        }
+        else{
+            $('#lnamealert').html("");
+            lnok = true;
+        }
+    });
     $('#register_submit').click(function(){
         let uname = $('#userReg').val();
-        let pw1 = $('#pw1Reg').val();
-        let pw2 = $('#pw1Reg').val();
+        let pw1 = $('#pw1Reg').val().trim();
         let fname = $('#fnameReg').val;
         let lname = $('#lnameReg').val;
         let email = $('#emailReg').val;
-        if (uname && pw1 && pw2 && fname && lname && email){
-            let uname = $('#userReg').val();
-            console.log(uname);
-        }
-        // if ($('#pw1Reg').val() === $('#pw2Reg').val()){
-        //     if
+        // if(email.includes("@")){
+        //     emailok = true;
         // }
+        console.log(pwok);
+        console.log(fnok);
+        console.log(lnok);
+        console.log(emailok);
+
+        if (pwok && fnok && lnok){
+            let obj = {"uname":uname,"password":pw1,"fname":fname,"lname":lname,"email":email};
+            users.push(obj);
+            console.log(users);
+        }
+        else{
+            console.log("attempt to register failed");
+        }
         
+    });
+    $('#login_submit').click(function(){
+        let uname = $('#username').val();
+        let pw1 = $('#password').val().trim();
+        let user = "pishoto";
+        if (uname && pw1){
+            users.forEach(element => {
+                if(element["uname"] == uname){
+                    let user = element;
+                }
+            });
+            if(pw1 == user["password"]){
+                window.alert("you are logged in");
+            }
+            else{
+                window.alert(user);
+            }
+        } 
     });
     var modal = document.getElementById("myModal");
 
