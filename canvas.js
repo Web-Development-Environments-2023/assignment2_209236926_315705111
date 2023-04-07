9682c63e559365f27fdd3657be8b9aa1a6694c33
Kentno <kentn@post.ac.il>
2023-04-07 19:49:46 +0300

    so anyway i started blasting

--- canvas.js
+++ canvas.js
@@ -7,17 +7,27 @@ $(document).ready(function () {
     // Set up the game state
     var y = canvas.height-50;
     var x = 524/2;
-    var img = new Image();
-    img.src = "assets/Ship_1.png";
+    var hero = new Image();
+    var enemy = new Image();
     var bg = new Image();
+    var fProj = new Image();
+    var eProj = new Image();
+    var weaponTimer = 0;
+    var trigger = false;
+    hero.src = "assets/Ship_1.png";
+    enemy.scr = "assets/Ship_5.png"
     bg.src = "assets/space_bg.jpg"
+    fProj.src = "assets/f_projectile.webp"
+    eProj.src = "assets/e_projectile.png"
+
     var start = false;
     var u = 0;
     var d = 0;
     var l = 0;
     var r = 0;
-    var enemy_shot_one = {x:0,y:0,active:false}
-    var enemy_shot_one = {x:0,y:0,active:false}
+    var enemyShotOne = {x:0,y:0,active:false}
+    var enemyShotTwo = {x:0,y:0,active:false}
+    var heroShots = [];
 
     // Set the refresh rate (in milliseconds)
     var refreshRate = 5;
@@ -26,11 +36,11 @@ $(document).ready(function () {
         
         // Clear the canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(bg, 0, 0, canvas.width, canvas.height)
+        ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
 
         // Draw the game state
 
-        // moveing logic
+        // moving logic
         y+=(d-u);
         x+=(r-l);
         if(y>=790){y=790;}
@@ -38,16 +48,37 @@ $(document).ready(function () {
         if(x>=525){x=525;}
         if(x<0){x=0;}
 
+        ctx.drawImage(hero, x, y);
 
 
-        ctx.drawImage(img, x, y);
-
+        // Process hero shooting logic
+        if (weaponTimer>0){weaponTimer-=1}
+        if (weaponTimer==0 && trigger) {
+            weaponTimer=100;
+            let velocity = 0;
+            if (l&&r){velocity = 0}
+            else if (r&&x!=525) {velocity = 1}
+            else if (l&&x!=0) {velocity = -1}
+            else {velocity = 0}
+            const temp = {X:x+10, Y:y-50, v:velocity, kill:false}
+            heroShots.push(temp)
+        }
+        
+        for (const shot of heroShots) {
+            shot.X+=shot.v;
+            shot.Y-=5;
+            ctx.drawImage(fProj,shot.X,shot.Y)
+            if( shot.Y<0 || shot.X>=540 || shot.X<0 || shot.Y>860){shot.kill = true}
+        }
+        heroShots = heroShots.filter(shot => !shot.kill)
+        
         // Schedule the next frame
         
         setTimeout(gameLoop, refreshRate);
         startButton.hidden = true;
 
     }
+    // Check if the key has been pressed, if so signal the main loop that the ship is moving in a direction
     document.addEventListener('keydown',function(event){
         if(event.key === "ArrowUp"){
             u=1;
@@ -61,9 +92,9 @@ $(document).ready(function () {
         if(event.key === "ArrowRight"){
             r=1;
         }
-
-        
+        if (event.key === " ") {trigger = true} 
     })
+    // Once a key is no longer pressed remove the command for the main loop that signals movement
     document.addEventListener('keyup',function(event){
         if(event.key === "ArrowUp"){
             u=0;
@@ -77,9 +108,24 @@ $(document).ready(function () {
         if(event.key === "ArrowRight"){
             r=0;
         }
+        if (event.key === " ") {trigger = false} 
     })
+
+
+    // check for fire button
 
     // Start the game loop
     startButton.onclick = gameLoop;
     function a(){}
+    function detectCollision(thisX, thisY, thisWidth, thisHeight, otherX, otherY, otherWidth, otherHeight) {
+        // Check for horizontal overlap
+        if (thisX + thisWidth >= otherX && thisX <= otherX + otherWidth) {
+          // Check for vertical overlap
+          if (thisY + thisHeight >= otherY && thisY <= otherY + otherHeight) {
+            // The objects are colliding
+            return true;
+          }
+        }
+        return false;
+      }
 });
